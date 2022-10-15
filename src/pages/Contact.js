f21614efcf423c4933e192683a24d9b75245a5f0
jeandtx <jean.doutriaux@gmail.com>
2022-10-15 22:28:43 +0800

    added local storage functionalities

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,81 @@
 import '../styles/Contact.css';
+import { useState } from 'react';
 
 function Contact() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [data, setData] = useState([]);
+
     return (
         <div className='contact'>
-            <div>
+            <div className='upper'>
                 <h1>Contact</h1>
-                <form>
-                    <input type="text" id="name" name="name" placeholder='name' />
-                    <input type="email" id="email" name="email" placeholder='email' />
-                    <input id="message" name="message" placeholder='Say something!' />
-                    <input type="submit" value="Submit" />
-                </form>
+                <div className='form'>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder='name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder='email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        id="message"
+                        name="message"
+                        placeholder='Say something!'
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
+                    <input
+                        type="submit"
+                        value="Submit"
+                        onClick={() => {
+                            setData([...data, { name, email, message }]);
+                            setName('Thank');
+                            setEmail('you');
+                            setMessage(';)');
+                        }}
+                    />
+                </div>
             </div>
             <div className='old-messages'>
                 <h3>Old Messages</h3>
-                <p>Message 1</p>
-                <p>Message 2</p>
+
+                {/* // todo choose between a table or differents div  */}
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Message</th>
+                    </tr>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.name}</td>
+                            <td>{item.email}</td>
+                            <td>{item.message}</td>
+                        </tr>
+                    ))}
+                </table>
+
+                {data.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            <p>NAME: {item.name}</p>
+                            <p>EMAIL: {item.email}</p>
+                            <p>MESSAGE: {item.message}</p>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     );
