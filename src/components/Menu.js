7521b8a473ff486f8b71aaa16bd07b90a8dcb9cb
jeandtx <jean.doutriaux@gmail.com>
2022-10-17 16:07:28 +0800

    styled menu's buttons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,10 +5,10 @@ function Menu() {
     return (
         <div className='Menu'>
             <div className='navbar'>
-                <div><a href='#header'>Home</a></div>
-                <div><a href='#aboutme'>About</a></div>
-                <div><a href='#projects'>Projects</a></div>
-                <div><a href='#contact'>Contact</a></div>
+                <div className='button'><a className='insidebutton' href='#header'>Home</a></div>
+                <div className='button'><a className='insidebutton' href='#aboutme'>About</a></div>
+                <div className='button'><a className='insidebutton' href='#projects'>Projects</a></div>
+                <div className='button'><a className='insidebutton' href='#contact'>Contact</a></div>
             </div>
         </div>
     );
